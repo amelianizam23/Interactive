f06674a93f14f79c8f1c3d964346ccdd4ad2d64c
Amelia Nizam <amelianizam23@gmail.com>
2024-07-12 11:58:25 -0700

    Initial Commit

---
+++ Interactive/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Interactive
+//
+//  Created by Scholar on 7/12/24.
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var textTitle = "What is your name?"
+    @State private var name = ""
+    
+    var body: some View {
+        VStack {
+           Text(textTitle)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+    
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Interactive/InteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveApp.swift
+//  Interactive
+//
+//  Created by Scholar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
